97041f5575a343c544ccc84fa2ef3681d63b9928
Eyal Abadi <eyalabadi@Eyals-MacBook-Pro.local>
2018-07-11 20:58:42 -0400

    Working Script for scheduler parser

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import os
 import os.path
 import numpy as np
 import datetime
+from numpy import nan as Nan
 
 print "\nStarting \n"
 lst = glob.glob("./schedule*")
@@ -36,10 +37,13 @@ print "Opened File \n"
 # print "DF "+ str(df1)
 teamShifts = {}
 daysOfWeek = [None] * 7
-
+totalshifts = {}
+listofTeams = {}
 firstTeam = list(df1)[0]
 print "First Team "+ str(firstTeam)
 current_team = str(firstTeam)
+df1.append(pd.Series([Nan], index=["Unnamed: 2"]), ignore_index=True)
+print "DF1 is" + str(df1)
 for i,row in df1.iterrows():
 
     try:
@@ -48,13 +52,21 @@ for i,row in df1.iterrows():
             current_team = row[0]
     except:
         print ""
-    if not row[0] == np.datetime64('NaT') and not row[0] == "" and not pd.isnull(df1.iloc[i,1]) and not row[0] == "Name":
+        #  and not pd.isnull(df1.iloc[i,1])
+    if not row[0] == np.datetime64('NaT')  and not row[0] == "Name":
         # print "First " + str(row[0])
+        temp  = {}
         if True:
-            if type(row["Unnamed: 2"]) == pd.Timestamp:
+            if type(row["Unnamed: 2"]) == pd.Timestamp and not row[0] == "":
+
                 dates = pd.to_datetime(row["Unnamed: 2"], format = '%Y%m%d')
                 date = dates.strftime('%Y-%m-%d')
-
+                print "Switching Teams" + current_team
+                listofTeams[i] = current_team
+                if row[0] == firstTeam:
+                    print "First team  appeared\n"
+                    teamShifts[0] = firstTeam
+            
                 # print "Time Found " + date + "\n" 
                 print "Current Team" + current_team
                 for number,day in enumerate(row):
@@ -63,17 +75,31 @@ for i,row in df1.iterrows():
                         continue
                     dates = pd.to_datetime(day, format = '%Y%m%d')
                     date = dates.strftime('%Y-%m-%d')
-                    print "Number "+ str(date)
                     daysOfWeek[number-1] = date
                 #print "Time " , row["Unnamed: 1"],row["Unnamed: 2"], row["Unnamed: 3"], "\n"
-                print "Days of Week "+ str(daysOfWeek)
+                # print "Days of Week "+ str(daysOfWeek)
             else:
-                
-                teamShifts[row[0]] = "Hello"
-                print "Current Team" + current_team
-                print row[0],row["Unnamed: 1"],row["Unnamed: 2"], row["Unnamed: 3"]
+                if pd.isnull(row[0]):
+                    print "Adding into totalshift: "+ current_team + ": "+ str(teamShifts)
+                    
+                    print " \nTotal Shifts up to team:" + str(totalshifts)
+                    print " \n Breaking! \n"
+                    temp = {}
+                    teamShifts = {}
+                    continue
+                print "Day " + str(row[0]) +": "+ str(current_team)
+                for num,day in enumerate(row):
+                    if not day == "Off" and not day == row[0]: 
+                        # print "Num -1:"+ str(daysOfWeek[num-1]) + " Index: "+str(num)
+                        # print "Days of Week " + str(daysOfWeek[0])
+                        temp[daysOfWeek[num-1]] = day
+                        teamShifts[row[0]] = temp
+                totalshifts[current_team + "Eyal"] = teamShifts
+                # daysOfWeek[0] = ""
+                # teamShifts[row[0]] = daysOfWeek[0]
+                # print "Current Team" + current_team
+                # print row[0],row["Unnamed: 1"],row["Unnamed: 2"], row["Unnamed: 3"]
                 #For each shift block   
-        print "\n TeamShofts: "+ str(teamShifts)
-        day
+print "\nTeamShifts: " + str(totalshifts)
 df1.head(8)
 df1.to_excel(file_name_schedule) #Write DateFrame back as Excel file
